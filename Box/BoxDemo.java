74ec75fbf4f30013bb34177e9426fd47176a55b5
Samruddhi Sunil Sur <samruddhisur08@gmail.com>
2026-01-11 20:04:16 +0530

    Create BoxDemo.java

---
+++ Box/BoxDemo.java
@@ -0,0 +1,20 @@
+class Box {
+    double width;
+    double height;
+    double depth;
+}
+
+class BoxDemo {
+    public static void main(String[] args) {
+        Box mybox = new Box();
+
+        mybox.width = 10;
+        mybox.height = 20;
+        mybox.depth = 15;
+
+        double vol;
+        vol = mybox.width * mybox.height * mybox.depth;
+
+        System.out.println("Volume is " + vol);
+    }
+}
