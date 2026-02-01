e3676979478674847a1c6d2a48f86e28eb96e370
Samruddhi Sunil Sur <samruddhisur08@gmail.com>
2026-02-01 20:28:21 +0530

    Add files via upload

---
+++ ImmutableClassDemo.java
@@ -0,0 +1,19 @@
+class ImmutableClass {
+    int a;
+
+    int add5() {
+        return a + 5;
+    }
+}
+
+public class ImmutableClassDemo {
+    public static void main(String[] args) {
+        ImmutableClass m1 = new ImmutableClass();
+        m1.a = 10;
+
+        int ans = m1.add5();
+
+        System.out.println("A in m1 = " + m1.a);
+        System.out.println("returned = " + ans);
+    }
+}
