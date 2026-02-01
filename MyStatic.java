9664bd220b17526b0a22ed3303afe586c4bba78d
Samruddhi Sunil Sur <samruddhisur08@gmail.com>
2026-02-01 20:14:31 +0530

    Add files via upload

---
+++ MyStatic.java
@@ -0,0 +1,14 @@
+public class MyStatic {
+
+    public static void main(String[] args) {
+        int a = 1, b = 2, c;
+
+        c = add(a, b); // static method call
+        System.out.println(c);
+    }
+
+    static int add(int i, int j) {
+        return i + j;
+    }
+}
+
