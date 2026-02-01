07b0bc902568fbd4e6458ff1400431793218f019
Samruddhi Sunil Sur <samruddhisur08@gmail.com>
2026-02-01 11:16:54 +0530

    Add files via upload

---
+++ SquareDemo.java
@@ -0,0 +1,11 @@
+class SquareDemo
+{
+    public static void main(String[] args)
+    {
+        int num = 5;
+        int square = num * num;
+
+        System.out.println("Number = " + num);
+        System.out.println("Square = " + square);
+    }
+}
